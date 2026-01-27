209fe2cfa9ddb66ee8965f14ab416c165978c4fe
Vera Skokova <miss.smiley17@gmail.com>
2026-01-27 11:29:11 +0700

    Fix: Add missing star import for io.ktor.client.statement to include bodyAsText

--- src/main/kotlin/client/UniversalGptClient.kt
+++ src/main/kotlin/client/UniversalGptClient.kt
@@ -8,7 +8,7 @@ import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.HttpResponse
+import io.ktor.client.statement.* // This includes HttpResponse and bodyAsText
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.*
