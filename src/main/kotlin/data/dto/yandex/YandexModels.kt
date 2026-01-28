57cd1a5e89f35cbc849afa7509ee55fa210b5141
Vera Skokova <miss.smiley17@gmail.com>
2026-01-28 23:05:16 +0700

    Create Yandex DTO models

---
+++ src/main/kotlin/data/dto/yandex/YandexModels.kt
@@ -0,0 +1,48 @@
+package data.dto.yandex
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class YandexRequest(
+    val modelUri: String,
+    val completionOptions: CompletionOptions,
+    val messages: List<YandexMessage>
+)
+
+@Serializable
+data class CompletionOptions(
+    val stream: Boolean = false,
+    val temperature: Double = 0.6,
+    val maxTokens: String = "2000"
+)
+
+@Serializable
+data class YandexMessage(
+    val role: String,
+    val text: String
+)
+
+@Serializable
+data class YandexResponse(
+    val result: YandexResult
+)
+
+@Serializable
+data class YandexResult(
+    val alternatives: List<YandexAlternative>,
+    val usage: YandexUsage
+)
+
+@Serializable
+data class YandexAlternative(
+    val message: YandexMessage,
+    val status: String
+)
+
+@Serializable
+data class YandexUsage(
+    val inputTextTokens: String,
+    val completionTokens: String,
+    val totalTokens: String
+)
